c3475f39d7062cec604878e9975e02147f0798e0
sakib6322 <132156251+sakib6322@users.noreply.github.com>
2023-04-29 23:46:47 +0600

    python code
    calclation of mathematics

---
+++ evalFuc.py
@@ -0,0 +1,21 @@
+# from speaker import talk
+# talk1 = "Enter your operation :"
+# talk2 = "Your result is :"
+# try:
+#     while True:
+#         print(eval(input(f"{talk1}{talk(talk1,120)}")))
+
+
+# except(ValueError,IndexError,):
+#     pass
+
+
+"""
+jlous life is possible for me it is allowed to life indu
+
+"""
+
+
+
+
+
